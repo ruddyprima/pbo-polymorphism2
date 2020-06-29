028d0bd2adaa5779a124b271cc850b839808e6d1
ruddyprima <primahendra70@gmail.com>
2020-06-29 19:11:07 +0700

    poligon

---
+++ src/polymorphism/poligon.java
@@ -0,0 +1,11 @@
+package polymorphism;
+
+public class poligon {
+    interface Poligon {
+        // Deklarasi methode
+        double Luas();
+        double Keliling();
+        String displayInfo();
+    }
+
+}
